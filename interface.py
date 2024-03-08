c74af3cfd58414441cc325f4ff7f8fb142f2ba15
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-03-08 10:15:34 +0100

    Obligé de push

---
+++ interface.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+import client
+
+ctk.set_appearance_mode("dark")
+
+ctk.set_default_color_theme("green")
+
+app = ctk.CTk()
+app.geometry("400x600")
+app.title("Modern Login UI using Customtkinter")
+
+app.mainloop()
